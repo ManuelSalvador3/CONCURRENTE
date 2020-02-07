9933656acdea908ed0b4f429170f54add58bacf3
ManuelSalvador3 <manusalvadorg@gmail.com>
2020-02-07 14:03:52 +0100

    Update WaitingRoom.java

--- WaitingRoom.java
+++ WaitingRoom.java
@@ -4,18 +4,19 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class WaitingRoom {
 
-    private final ArrayBlockingQueue<Customer> waitingCustomers;
+    private final ArrayBlockingQueue<Customer> waitingCustomers; //Cola implementada mediante un array
 
-    public WaitingRoom(int capacity) {
+    public WaitingRoom(int capacity) { //Inicializamos los atributos
         waitingCustomers = new ArrayBlockingQueue<>(capacity);
     }
-
+    
+    // Funcion que hace que el cliente se siente en una silla a esperar.
     public void takeASeat(Customer customer) throws InterruptedException {
         if (waitingCustomers.remainingCapacity() == 0)
             //System.out.println("El cliente "+ customer + "ha llegado pero no tiene donde esperar.");
         waitingCustomers.put(customer);
     }
-
+     // Funcion que devuelve el siguiente cliente esperando
     public Customer nextCustomer() throws InterruptedException {
         return waitingCustomers.take();
     }

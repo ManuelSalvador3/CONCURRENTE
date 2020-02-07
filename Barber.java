eb88d0863b309c5fbfdb79660a3c538080ed4814
ManuelSalvador3 <manusalvadorg@gmail.com>
2020-02-07 14:07:56 +0100

    Update Barber.java

--- Barber.java
+++ Barber.java
@@ -4,7 +4,7 @@ public class Barber implements Runnable {
 
     private final WaitingRoom waitingRoom;
 
-    public Barber(WaitingRoom waitingRoom) {
+    public Barber(WaitingRoom waitingRoom) { // Se inicializan los atributos de la clase
         this.waitingRoom = waitingRoom;
     }
 
@@ -12,11 +12,12 @@ public class Barber implements Runnable {
     public void run() {
         try {
             while (true) {
+                // Se asigna a customer el siguiente en la cola de espera
                 Customer customer = waitingRoom.nextCustomer();
-
+                // Se le llama a cortarse el pelo
                 System.out.println("---- El barbero llama al cliente " + customer + " y le empieza a cortar el pelo. ----");
-                Thread.sleep(2000);
-                customer.callAndShave();
+                Thread.sleep(2000); // Duerme 2000ms y cuando pasa el tiempo ha acabado el corte de pelo
+                customer.callAndShave(); // 
             }
 
         } catch (InterruptedException e) {

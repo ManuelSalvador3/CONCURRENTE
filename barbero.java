c4dc6d402d0494f17ac66e4487660f62f9ec52b5
ManuelSalvador3 <manusalvadorg@gmail.com>
2020-02-07 11:40:07 +0100

    Update barbero.java

--- barbero.java
+++ barbero.java
@@ -11,33 +11,33 @@ package edu.distrital.log.main;
 //   de espera.
 public class barberia {
 
-	private int nSillasEspera;
-	private int nSillasEsperaOcupadas = 0;
-	private boolean sillaBarberoOcupada = false;
-	private boolean finCorte = false;
-	private boolean barberoDormido = false;
+	private int nSillasEspera; //Atributo que representa las sillas de espera totales
+	private int nSillasEsperaOcupadas = 0; // Atributo que representa las silla de espera ocupadas
+	private boolean sillaBarberoOcupada = false; //Atributo que dice si la silla del barbero esta ocupada o no
+	private boolean finCorte = false; // Atributo que dice si el corte de pelo al cliente ha acabado o no
+	private boolean barberoDormido = false; // Atributo que dice si el barbero esta dormido o no
 
 	// JAVA: sólo puede haber N_Sillas_Espera_max hebras
 	// esperando dentro del monitor a que le toque.
 
-	public barberia(int nSillasEspera) {
+	public barberia(int nSillasEspera) { //Inicicializamos las variables
 		this.nSillasEspera = nSillasEspera;
 	}
 
 	public synchronized boolean entrar(int clienteId)
 			throws InterruptedException {
-		if (nSillasEsperaOcupadas == nSillasEspera) {
-			// Si no hay sillas libres, me voy sin cortar el pelo
+		if (nSillasEsperaOcupadas == nSillasEspera) { 
+			// Si las sillas ocupadas igual a las totales me voy sin cortar el pelo
 			System.out.println("---- El cliente " + clienteId
 					+ " se va sin cortarse el pelo");
 			return false;
 		} else {
-			// Me quedo esperando si la silla del barbero está
-			// ocupada
-			nSillasEsperaOcupadas++;
+			// Si no estan todas ocupadas me siento a esperar
+			nSillasEsperaOcupadas++; //Aumenta en 1 las sillas ocupadas
 			System.out.println("---- El cliente " + clienteId
 					+ " se sienta en la silla de espera");
 			while (sillaBarberoOcupada) {
+			// Mientras la silla del barbero esta ocupada esperamos
 				wait();
 			}
 
@@ -59,9 +59,10 @@ public class barberia {
 			System.out.println("---- El cliente " + clienteId
 					+ " en la silla de barbero");
 			while (!finCorte) {
+			// Mientras el corte de pelo no haya acabado, esperamos
 				wait();
 			}
-
+			// Queda libre la silla del barbero
 			sillaBarberoOcupada = false;
 
 			// Que pase el siguiente
@@ -73,10 +74,9 @@ public class barberia {
 		}
 	}
 
+	// El barbero espera a que llegue un cliente. Mientras la silla del barbero no este
+	// ocupada el barbero espera durmiendo.
 	public synchronized void esperarCliente() throws InterruptedException {
-		// El barbero espera a que llegue un cliente
-		// Se supone que le corta el pelo fuera del
-		// monitor
 		barberoDormido = true;
 		while (!sillaBarberoOcupada) {
 			System.out.println("++++ Barbero esperando cliente");
@@ -85,7 +85,9 @@ public class barberia {
 		barberoDormido = false;
 		System.out.println("++++ Barbero cortando el pelo");
 	}
-
+	
+	// Si ha acabado el corte de pelo finCorte= true y se notifica a todos los 
+	// que estan esperando de que ha acabado
 	public synchronized void acabarCorte() {
 		finCorte = true;
 		System.out.println("++++ Barbero termina de cortar el pelo");
